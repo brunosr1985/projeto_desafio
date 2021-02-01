cbd49ff1ced9a1d78a4035b13b86204da90ebff3
Bruno dos Rodrigues <bruno.rodrigues@enforcegroup.com.br>
2021-01-31 21:48:24 -0300

    Adicionar arquivos de projeto.

---
+++ backend/Web Charge/Examples.Charge.Application/AutoMapper/PersonProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Examples.Charge.Application.Dtos;
+using Examples.Charge.Domain.Aggregates.PersonAggregate;
+
+namespace Examples.Charge.Application.AutoMapper
+{
+    public class PersonProfile : Profile
+    {
+        public PersonProfile()
+        {
+            CreateMap<Person, PersonDto>()
+               .ReverseMap()
+               .ForMember(dest => dest.BusinessEntityID, opt => opt.MapFrom(src => src.BusinessEntityID));
+        }
+    }
+}
---
+++ backend/Web Charge/Examples.Charge.Application/Dtos/PersonDto.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Events.Bus;
+using Examples.Charge.Domain.Aggregates.PersonAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.Charge.Application.Dtos
+{
+    public class PersonDto
+    {
+        public int BusinessEntityID { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<PersonPhone> Phones { get; set; }
+
+        //public ICollection<IEventData> DomainEvents => throw new NotImplementedException();
+    }
+}
